3dc80c02c0d68d6ef1166788a06b542d8446c3a5
sebs-commits <121909326+sebs-commits@users.noreply.github.com>
2025-04-06 15:25:57 -0400

    refactor: news model, resolver, typedef rework

--- server/microservices/community-engagement-microservice/models/news.server.model.js
+++ server/microservices/community-engagement-microservice/models/news.server.model.js
@@ -3,20 +3,15 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const newsSchema = new Schema({
-    creatorId: { type: String, required: true },//{ type: Schema.Types.ObjectId, ref: "User" }
+  creatorId: { type: String, required: true }, //{ type: Schema.Types.ObjectId, ref: "User" }
 
-    headline: { type: String, required: true },        
+  headline: { type: String, required: true },
 
-    summary: { type: String, required: true },
+  textBody: { type: String, required: true },
 
-    fullnews: { type: String, required: true },
+  creationDate: { type: Date, default: Date.now, required: true },
+});
 
-    creationDate: { type: String, required: true },
-
-    expiryDate: { type: String, required: true }
-})
-
-const NewsModel = mongoose.model('News', newsSchema);
+const NewsModel = mongoose.model("News", newsSchema);
 
 export default NewsModel;
-
--- server/microservices/community-engagement-microservice/schemas/news-typedefs.js
+++ server/microservices/community-engagement-microservice/schemas/news-typedefs.js
@@ -1,41 +1,29 @@
 import { gql } from "apollo-server-express";
 
 export const newsTypeDefs = gql`
-    type News {
-        _id: ID!
-        creatorId: String!
-        headline: String!
-        summary: String!
-        fullnews: String!
-        creationDate: String!
-        expiryDate: String!
-    }
+  type News {
+    _id: ID!
+    creatorId: String!
+    headline: String!
+    textBody: String!
+    creationDate: String!
+  }
 
-    type Query {
-        allNoneExpiredNews: [News]
-        news(_id: ID!): News
-    }
+  type Query {
+    allNews: [News]
+    news(_id: ID!): News
+  }
 
-    type Mutation {
-        createNews(
-            creatorId: String!,
-            headline: String!,
-            summary: String!,
-            fullnews: String!,
-            creationDate: String!,
-            expiryDate: String!
-        ): News
+  type Mutation {
+    createNews(creatorId: String!, headline: String!, textBody: String!): News
 
-        updateNews(
-            _id: ID!,
-            creatorId: String!,
-            headline: String!,
-            summary: String!,
-            fullnews: String!,
-            creationDate: String!,
-            expiryDate: String!
-        ): News
+    updateNews(
+      _id: ID!
+      creatorId: String!
+      headline: String!
+      textBody: String!
+    ): News
 
-        deleteNews(_id: ID!): News
-    }
+    deleteNews(_id: ID!): News
+  }
 `;
