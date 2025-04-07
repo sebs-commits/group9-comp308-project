1ede0fda414691ddd12aa4907d64a2b42df8d373
Sebastian <sebastian_bdavid@hotmail.com>
2025-04-07 11:36:38 -0400

    refactor: updated create news mutation and create news component, renamed CreateUpdateNews to CreateNews

--- client/community-business-app/src/main.jsx
+++ client/community-business-app/src/main.jsx
@@ -1,12 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
 
+// We need to combine two URIs here
 const link = createHttpLink({
-  uri: 'http://localhost:4002/graphql',
-  credentials: 'include'
+  uri: "http://localhost:4003/graphql", // Community Engagement Microservice, we need to also include business events etc
+  credentials: "include",
 });
 
 const client = new ApolloClient({
@@ -14,10 +20,10 @@ const client = new ApolloClient({
   link,
 });
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-     <ApolloProvider client={client}>
+    <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
   </StrictMode>
-)
+);
