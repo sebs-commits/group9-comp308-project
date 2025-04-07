4415b490b64695389ac6757e6163dcceb18e0a5b
sebs-commits <121909326+sebs-commits@users.noreply.github.com>
2025-04-07 17:12:47 -0400

    fix: renamed import for CreateNews

--- client/community-business-app/src/App.jsx
+++ client/community-business-app/src/App.jsx
@@ -1,46 +1,57 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import Container from 'react-bootstrap/Container';
-import React from 'react';
-import CreateUpdateNewsComponent from './components/CreateUpdateNews';
-import CreateUpdateRequestComponent from './components/CreateUpdateRequests';
-import CreateUpdateAlertComponent from './components/CreateUpdateAlerts';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import React from "react";
+import CreateUpdateNewsComponent from "./components/CreateNews";
+import CreateUpdateRequestComponent from "./components/CreateUpdateRequests";
+import CreateUpdateAlertComponent from "./components/CreateUpdateAlerts";
 
 function App() {
   return (
     <Router>
-        <div>
-          <header>
-            <Navbar bg="secondary" variant="dark" expand="lg">
-              <Container>
-                <Navbar.Brand as={Link} to={"/news"}>News</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                  <Nav className="mr-auto">
-                    <Nav.Link as={Link} to="/news">News</Nav.Link>
-                    <Nav.Link as={Link} to="/requests">Requests</Nav.Link>
-                    <Nav.Link as={Link} to="/alerts">Alerts</Nav.Link>
-                  </Nav>
-                </Navbar.Collapse>
-              </Container>
-            </Navbar>
+      <div>
+        <header>
+          <Navbar bg="secondary" variant="dark" expand="lg">
+            <Container>
+              <Navbar.Brand as={Link} to={"/news"}>
+                News
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                  <Nav.Link as={Link} to="/news">
+                    News
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/requests">
+                    Requests
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/alerts">
+                    Alerts
+                  </Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
 
-            <div>
+          <div>
             <Routes>
-                  <Route index element={<CreateUpdateNewsComponent />} />
-                  <Route path="news" element={<CreateUpdateNewsComponent />} />
-                  <Route path="requests" element={<CreateUpdateRequestComponent/>}/>
-                  <Route path="alerts" element={<CreateUpdateAlertComponent/>}/>
-                </Routes>
-            </div>
-          </header>
-        </div>
-      </Router>
-  )
+              <Route index element={<CreateUpdateNewsComponent />} />
+              <Route path="news" element={<CreateUpdateNewsComponent />} />
+              <Route
+                path="requests"
+                element={<CreateUpdateRequestComponent />}
+              />
+              <Route path="alerts" element={<CreateUpdateAlertComponent />} />
+            </Routes>
+          </div>
+        </header>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
