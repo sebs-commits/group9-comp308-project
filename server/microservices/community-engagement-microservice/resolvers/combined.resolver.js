c16119f28da79f73fd799b2cf17f1775446a7cc4
sebs-commits <121909326+sebs-commits@users.noreply.github.com>
2025-04-10 20:21:57 -0400

    feat: combined discussions resolver

--- server/microservices/community-engagement-microservice/resolvers/combined.resolver.js
+++ server/microservices/community-engagement-microservice/resolvers/combined.resolver.js
@@ -1,19 +1,22 @@
 import { alertResolvers } from "./alert.server.resolver.js";
 import { newsResolvers } from "./news.server.resolver.js";
 import { requestsResolvers } from "./requests.server.resolver.js";
+import { discussionsResolvers } from "./discussions.resolver.js";
 
 export const combinedResolvers = {
     Query: {
         _microservice: () => "This is a micro service query",
         ...newsResolvers.Query,
         ...requestsResolvers.Query,
-        ...alertResolvers.Query
+        ...alertResolvers.Query,
+        ...discussionsResolvers.Query
 
     },
 
     Mutation: {
         ...newsResolvers.Mutation,
         ...requestsResolvers.Mutation,
-        ...alertResolvers.Mutation     
+        ...alertResolvers.Mutation,
+        ...discussionsResolvers.Mutation
     }
 }
