a243c1a2c21c57a5442b69f6a31a222222c73e6b
Sebastian <sebastian_bdavid@hotmail.com>
2025-04-13 17:16:58 -0400

    feat/refactor: added GET_REQUESTS, reformatted page

--- client/community-business-app/shared/gql/request.gql.js
+++ client/community-business-app/shared/gql/request.gql.js
@@ -1,13 +1,37 @@
-import { gql } from "@apollo/client"
+import { gql } from "@apollo/client";
 
 export const CREATE_REQUEST = gql`
-    mutation CreateRequest($creatorId: String!, $title: String!, $type: String!, $request: String!, $location: String!) {
-        createRequest(creatorId: $creatorId, title: $title, type: $type, request: $request, location: $location) {
-            creatorId
-            title
-            type
-            request
-            location
-        }
+  mutation CreateRequest(
+    $creatorId: String!
+    $title: String!
+    $type: String!
+    $request: String!
+    $location: String!
+  ) {
+    createRequest(
+      creatorId: $creatorId
+      title: $title
+      type: $type
+      request: $request
+      location: $location
+    ) {
+      creatorId
+      title
+      type
+      request
+      location
     }
+  }
+`;
+export const GET_REQUESTS = gql`
+  query GetRequests {
+    requests {
+      id
+      creatorId
+      title
+      type
+      request
+      location
+    }
+  }
 `;
