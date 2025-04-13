f83463629bee1e832b09a2d9fc1c073b69f70e0f
Sebastian <sebastian_bdavid@hotmail.com>
2025-04-13 19:34:09 -0400

    style: style ViewAllRequest component

--- client/community-business-app/src/components/ViewAllRequest.jsx
+++ client/community-business-app/src/components/ViewAllRequest.jsx
@@ -6,6 +6,7 @@ import Spinner from "react-bootstrap/Spinner";
 import Alert from "react-bootstrap/Alert";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 
 const ViewAllRequest = () => {
   const { loading, error, data } = useQuery(GET_REQUESTS);
@@ -36,27 +37,31 @@ const ViewAllRequest = () => {
   }
 
   return (
-    <div className="px-5 pb-4">
-      <h4 className="pt-4 pb-2 text-center">All Requests</h4>
+    <Container fluid className="text-light">
+      <h4 className="pt-4 pb-3 text-center">All Requests</h4>
       <Row className="g-4">
         {requests.map((request) => (
-          <Col key={request.id} md={4} sm={6}>
-            <Card>
-              <Card.Body>
-                <Card.Title>{request.title}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">
-                  {request.type}
+          <Col key={request.id} xl={3} lg={4} md={6} sm={6} className="mb-4">
+            <Card className="bg-dark text-light h-100 border border-secondary rounded-4 opacity-75">
+              <Card.Body className="d-flex flex-column">
+                <Card.Title className="text-start fw-bold">
+                  {request.title}
+                </Card.Title>
+                <Card.Subtitle className="mb-2 text-start text-light-emphasis">
+                  {request.type.charAt(0).toUpperCase() + request.type.slice(1)}
                 </Card.Subtitle>
-                <Card.Text>{request.request}</Card.Text>
-                <Card.Footer className="text-muted">
+                <Card.Text className="text-start flex-grow-1 medium">
+                  {request.request}
+                </Card.Text>
+                <div className="text-start text-light mt-auto small">
                   Location: {request.location}
-                </Card.Footer>
+                </div>
               </Card.Body>
             </Card>
           </Col>
         ))}
       </Row>
-    </div>
+    </Container>
   );
 };
 
