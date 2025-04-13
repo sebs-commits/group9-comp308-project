791f49ac5e3f5fa3ae197f1025357db066f23f96
Sebastian <sebastian_bdavid@hotmail.com>
2025-04-13 17:05:07 -0400

    refactor: added location value to request model

--- server/microservices/community-engagement-microservice/models/requests.server.model.js
+++ server/microservices/community-engagement-microservice/models/requests.server.model.js
@@ -15,6 +15,14 @@ const requestsSchema = new Schema({
         },
         required: true
     },
+    location: {
+        type: String,
+        enum: {
+            values: [ "Toronto", "Mississauga", "Markham", "Kingston", "Oakville", "Ajax"],
+            message: '{VALUE} is not supported'
+        },
+        required: true
+    },
     
     request: { type: String, required: true }
 })
