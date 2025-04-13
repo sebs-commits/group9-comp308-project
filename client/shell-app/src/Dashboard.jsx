c407123d1710801fff511be162f2245ce953d009
Audrey Bernier Larose <abernier@my.centennialcollege.ca>
2025-04-13 13:15:06 -0400

    [feat.] Organizer Dashboard UI

--- client/shell-app/src/Dashboard.jsx
+++ client/shell-app/src/Dashboard.jsx
@@ -2,12 +2,13 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import { useState } from 'react';
 import { CommunityOrganizerDashboardComponent, ResidentDashboardComponent, BusinessOwnerDashboardComponent} from './Dashboards';
+import { Label } from '../shared/resources';
 
 const Dashboard = () => {
     const [type, setType] = useState(sessionStorage.getItem('type') || '');
 
     return <> 
-        <Container className="py-5 justify-content-center">
+        <Container className="justify-content-center">
             <Row xs={12} sm={12} md={8} lg={8} xl={8} className="justify-content-center">
                 
                 { type === 'organizer' && <CommunityOrganizerDashboardComponent/> }
@@ -18,6 +19,10 @@ const Dashboard = () => {
                                     
             </Row>
         </Container>
+
+        <div style={{ backgroundColor: "#6c757d", height: "50px" }}>
+             <p style={{fontWeight: "bold", paddingTop:"15px", fontSize:"12px", color: "#fecd00"}}>&copy; {Label.COPYRIGHT}</p>
+        </div>
     </>
 }
 
